a18ffb38c862614389573ef9d9120125b277304e
barkowal <b.kowalski1@o365.us.edu.pl>
2026-03-27 20:08:07 +0100

    Init

---
+++ backend/src/main/java/com/barkowal/nundine/NundineApplication.java
@@ -0,0 +1,13 @@
+package com.barkowal.nundine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NundineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NundineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/barkowal/nundine/NundineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barkowal.nundine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NundineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
