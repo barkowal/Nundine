079aa2be85696e66102f7000e2b9151b69133bca
barkowal <b.kowalski1@o365.us.edu.pl>
2026-04-08 19:44:37 +0200

    Fix warnings

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationBar } from './components/navigation-bar/navigation-bar';
-import { HomePage } from './pages/home-page/home-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationBar, HomePage],
+  imports: [RouterOutlet, NavigationBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
